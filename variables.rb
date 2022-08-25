8a6f0ea281d4ee21bf4dfed16563064280a9faa7
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-08-24 21:24:44 -0700

    Reassigns strings and adds puts

--- variables.rb
+++ variables.rb
@@ -13,3 +13,12 @@ cash = 25.00
 alive = true
 sad = false
 hungry = false
+
+name = 'Kaylah Rose'
+home = 'away'
+ball = 'still life'
+
+puts 'mike check'
+puts 'testing 1,2'
+puts 'I am a computer'
+puts 'beep boop bop'
